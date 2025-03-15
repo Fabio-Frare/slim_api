9cf93bf4da0974b22051a7e9e154642bc3ff33ce
Fabio-Frare <44546034+Fabio-Frare@users.noreply.github.com>
2025-03-15 17:28:07 -0300

    Implementado rotas para produtos

--- db.php
+++ db.php
@@ -22,23 +22,25 @@ $schema->create($tabela, function($table) {
     $table->text('descricao', 100);
     $table->decimal('preco', 11, 2);
     $table->string('fabricante', 60);
-    $table->date('dt_criacao');
+    $table->timestamps();
 });
 
 $db->table($tabela)->insert([
-    'titulo'      => 'Smatphone Motorola Moto G6',
-    'descricao'   => 'Android Oreo - 8.0 Tela 5.7',
-    'preco'       => 899.00,
-    'fabricante'  => 'Motorola',
-    'dt_criacao' => '2019-10-22'
+    'titulo'     => 'Smartphone Motorola Moto G6',
+    'descricao'  => 'Android Oreo - 8.0 Tela 5.7',
+    'preco'      => 899.00,
+    'fabricante' => 'Motorola',
+    'created_at' => '2019-10-22',
+    'updated_at' => '2019-10-22'
 ]);
 
 $db->table($tabela)->insert([
-    'titulo'      => 'Iphone X Cinza 64G',
-    'descricao'   => ' Tela 5.8 IOS 12',
-    'preco'       => 4500.00,
-    'fabricante'  => 'Apple',
-    'dt_criacao' => '2020-01-10'
+    'titulo'     => 'Iphone X Cinza 64G',
+    'descricao'  => ' Tela 5.8 IOS 12',
+    'preco'      => 4500.00,
+    'fabricante' => 'Apple',
+    'created_at' => '2020-01-10',
+    'updated_at' => '2020-01-10'
 ]);
 
 
--- src/routes.php
+++ src/routes.php
@@ -4,4 +4,5 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 
 // Routes
+require __DIR__ . '/routes/produtos.php';
 
